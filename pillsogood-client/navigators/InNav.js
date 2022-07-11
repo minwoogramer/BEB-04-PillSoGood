e83e43aaf60f2dafe583b7b38f67dd40d7b9b25a
minwoogramer <01082249606nmw@gmail.com>
2022-07-12 03:29:04 +0900

    Feat:Footer 세팅, Header 완료

--- pillsogood-client/navigators/InNav.js
+++ pillsogood-client/navigators/InNav.js
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-
+import Home from "../screens/Home";
 const Nav = createNativeStackNavigator();
 
 const InNav = () => (
--- pillsogood-client/navigators/OutNav.js
+++ pillsogood-client/navigators/OutNav.js
@@ -1,11 +1,19 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import { BASE_COLOR } from "../colors";
 import Login from "../screens/Login";
 import Register from "../screens/Register";
 const Nav = createNativeStackNavigator();
 
 const OutNav = () => (
-  <Nav.Navigator>
+  <Nav.Navigator
+    screenOptions={{
+      headerTintColor: "black",
+      headerStyle: {
+        backgroundColor: BASE_COLOR,
+      },
+    }}
+  >
     <Nav.Screen name="Login" component={Login} />
     <Nav.Screen name="Register" component={Register} />
   </Nav.Navigator>
